59ed256860bcd4aa24ae77174575912c9cd74d46
kakinha <karina.corrotti@gmail.com>
2020-07-18 11:05:42 -0300

    serverDoProjeto

---
+++ lojaOnlineServer/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const db = require('./mysql/db');
+const cors = require('cors');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({credentials: true, origin: 'http://localhost:3030'}));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
+
+app.listen(3333, () => {
+  console.log('servidor ok');
+  db.authenticate().then(() =>{
+      console.log('Conectou no MYSQL')
+  }).catch(() =>{
+      console.log('Erro ao se conectar no MYSQL')
+  })
+});
